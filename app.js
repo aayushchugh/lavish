2ba514ce9f27e1b167474144252e733d2dc48127
Ayush Chugh <ayushchugh2006@gmail.com>
2021-11-22 15:11:21 +0530

    heroku config

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
@@ -11,5 +12,13 @@ app.use(apiAuth);
 
 /* --------------------------------- routes --------------------------------- */
 app.use('/api', authRouter);
+
+/* ---------------------- render html from build folder --------------------- */
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 module.exports = app;
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = require('./app');
 
 dotenv.config();
 
+const PORT = process.env.PORT || 8000;
+
 connect(`${process.env.DB_URI}`, () => {
-	app.listen(8000);
+	app.listen(PORT);
 });
