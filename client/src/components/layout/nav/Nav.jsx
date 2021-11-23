accf95fa076f38ad93b6ccab7d4b71dba42e8fcf
Ayush Chugh <ayushchugh2006@gmail.com>
2021-11-23 18:27:00 +0530

    nav - client

--- client/src/components/layout/nav/Nav.jsx
+++ client/src/components/layout/nav/Nav.jsx
@@ -5,6 +5,9 @@ import Search from '@mui/icons-material/Search';
 import PersonIcon from '@mui/icons-material/Person';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
+import './Nav.scss';
+import { red } from '@mui/material/colors';
+
 const Nav = () => {
 	return (
 		<header>
@@ -16,40 +19,40 @@ const Nav = () => {
 				</div>
 
 				<ul className="nav__links">
-					<li className="nav__link">
+					<li className="nav__link nav__link--1">
 						<Link to="/">Home</Link>
 					</li>
 
-					<li className="nav__link">
+					<li className="nav__link nav__link--2">
 						<Link to="/shop">Shop</Link>
 					</li>
 
-					<li className="nav__link">
+					<li className="nav__link nav__link--3">
 						<Link to="/deals">Today Deals</Link>
 					</li>
 
-					<li className="nav__link">
+					<li className="nav__link nav__link--4">
 						<Link to="/shop/cloths">Cloths</Link>
 					</li>
 
-					<li className="nav__link">
+					<li className="nav__link nav__link--5">
 						<Link to="/shop/jewelry">Jewelry</Link>
 					</li>
 
-					<li className="nav__link">
-						<TextField variant="filled" label="Search" />
+					<li className="nav__link nav__search">
+						<input type="text" placeholder="Search" />
 						<Search />
 					</li>
 				</ul>
 
 				<ul className="nav__buttons">
-					<Tooltip title="cart">
+					<Tooltip title={<h1>Cart</h1>} arrow>
 						<IconButton>
 							<ShoppingCartIcon />
 						</IconButton>
 					</Tooltip>
 
-					<Tooltip title="My Account">
+					<Tooltip title={<h1>Account</h1>} arrow>
 						<IconButton>
 							<PersonIcon />
 						</IconButton>
